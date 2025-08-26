4f6aaf605012c35681798890384eb673ed6b0762
cheickbaws223-sudo <cheickbaws223@gmail.com>
2025-08-26 18:51:56 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0VvT1ZJU1k2azhGNTZib1pOMEh4TmNQTGlRYkJyQ3g1emJsd0NycW1VZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTFNaekVtMEhEUXNZUGI2WkZ1UGJxa3NrSFNiUzhmY20zNDFaM2hqQjZVWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDRDdpOXF6ajlsVlR6OEdseStNcmQyMG1MTGRRTmdRRVZKR01Bb0ZYbDFRPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIvS2pEU2U3TzZ3U2x4a3c4QXY3djExYkQwNWlpTXZkK3VIQUs0dnpJTkg4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IitOTFZoWVdXaktadmFnbHhsblFIamt0eEpWcVpVbmZ2d1lya3dEVlJCVTg9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkdaOENpUG1ucUxrSURXVlUxbjdmWEhDNlZIaTdKVXVpVjY1YmtrbEwzbDQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOE5XQk1kR2REWCtPT1JWeGtUYlM4ZXNYcDY5N1g5WlhsVE42QnZTMkExWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK3ZJVmxPZVZ1OHBTL1lpbjhEbkFzWWYwbklzSHRqTHp6bFhSaDQ4SW8wND0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InoySnFqdTBTTDVBby9EVXY4L2hxZndTemtlSmhMK2RzKzRKZDV2cC9meXJNVlU0cUkrS2tISHY2dEdmUnphU3I0OHlFaDdiN2tTbkRtRlo3aHZQZ0FnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjIyLCJhZHZTZWNyZXRLZXkiOiJKNVlmbFhaQmlpQ2JabHU4NjJOQ3VCREM1dkJqN1VobkFKTC9uQm8rVE1JPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6IjIyNTA2MTk4MTQ2QHMud2hhdHNhcHAubmV0IiwiZnJvbU1lIjp0cnVlLCJpZCI6IjNCNDY3MjRBNTMxMTFGNDY4RDQ1N0UzNjYwRjkwRjA1In0sIm1lc3NhZ2VUaW1lc3RhbXAiOjE3NTYyMzQwODl9XSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IkM0NlJaN1BEIiwibWUiOnsiaWQiOiIyMjUwNjE5ODE0NjozM0BzLndoYXRzYXBwLm5ldCIsImxpZCI6IjEzMDkzMjc2NTg4ODYyMDozM0BsaWQiLCJuYW1lIjoi44OB44Kn44OJ44Oq44O844GP44KT8J+Rge+4j+KAjfCfl6jvuI8ifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ09uMHRiNENFTitDdU1VR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InNPUmU1d1FpTUdJSTNGdG10UW5DWjZFWHBoellYR1lMd0M0Wno1YXVwQ3M9IiwiYWNjb3VudFNpZ25hdHVyZSI6Ikx5ZnJCZzM4YTZnTFM4NlNBSDcvMVBvZG5NVG5hNk1IUVM5MmQwbUwzTGhTRXV4dnBIazBnMlA3OG1EYnpkeXhQa1MxRlA0NmZ3dStJdFk4RDVoakN3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJ5b2ppaHFXcXZ0ZFRpcVFDL0lmWTlVc1MzeERLekNGL3lqeWQycnpRb0RRNHZXYjhIamsyRzlTODl6S09BZWRQajJ6bHg4aFBKVzlkbzgzRGFJOTRDUT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIyNTA2MTk4MTQ2OjMzQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmJEa1h1Y0VJakJpQ054YlpyVUp3bWVoRjZZYzJGeG1DOEF1R2MrV3JxUXIifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBZ0lCUT09In0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc1NjIzNDA4MywibGFzdFByb3BIYXNoIjoiMlAxWWhmIiwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFOZkoifQ==",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -33,9 +33,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923059395959",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "+2250506198146",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "MR-SHABAN",
+OWNER_NAME: process.env.OWNER_NAME || "CHEICK BAWS",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ*",
 // add bot owner name    
@@ -63,11 +63,11 @@ ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923043788282",
+DEV: process.env.DEV || "+2250506198146",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "false",
 // true for anti once view
